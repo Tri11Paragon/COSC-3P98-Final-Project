7d4477b983a40e7abcd9f196064157728b10528b
Brett Laptop <tri11paragon@tpgc.me>
2023-01-16 14:12:53 -0500

    Update BLT

---
+++ include/window/window.h
@@ -0,0 +1,18 @@
+/*
+ * Created by Brett Terpstra 6920201 on 16/01/23.
+ * Copyright (c) Brett Terpstra 2023 All Rights Reserved
+ */
+
+#ifndef FINAL_PROJECT_WINDOW_H
+#define FINAL_PROJECT_WINDOW_H
+
+#include <blt/window/window.h>
+
+class glut_window : public blt::window {
+private:
+
+public:
+    
+};
+
+#endif //FINAL_PROJECT_WINDOW_H
